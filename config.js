b158637c038320643d01202742679f28a48a84fa
swethakothapalli <swetha.kothapalli@outlook.com>
2020-12-04 13:12:50 +0530

    changed_titile

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by Swetha Kothapalli',
+  subtitle: 'Data Analyst having 2years experience',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Swetha Kothapalli',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Data Analyst having 2years experience',
     contacts: {
       email: '',
       facebook: '#',
